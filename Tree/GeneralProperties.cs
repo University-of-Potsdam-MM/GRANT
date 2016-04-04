b9632e196f7d7071a2cf435c65f62dec5f822659
mazandar <mazandar@uni-potsdam.de>
2016-04-04 14:07:40 +0200

    Properties Liste

--- Tree/GeneralProperties.cs
+++ Tree/GeneralProperties.cs
@@ -9,30 +9,140 @@ namespace Tree
 {
     public struct GeneralProperties
     {
-        public String Name
-        {
-            get;
-            set;
-        }
 
+       
         //TODO: evtl. Enum für ControlType erstellen
-        public String LocalizedControlType
+        public String controlsTypeFiltered
         {
             get;
             set;
         }
 
-        public Rect BoundingRectangle
+        //Property: isEnabled
+        // Boolean? => true, false, null
+        public Boolean? disabedFiltered
+        {
+            get;
+            set;
+        }
+        //Property: BoundingRectangle
+        public Rect boundingRectangleFiltered
+        {
+            get;
+            set;
+        }
+
+        //Property: Name
+        public String nameFiltered
+        {
+            get;
+            set;
+        }
+
+        //Property: AutoamtionId
+        public int IdFiltered
+        {
+            get;
+            set;
+        }
+
+        public int IdGenerated
+        {
+            get;
+            set;
+        }
+
+        public Boolean? checkedFiltered
+        {
+            get;
+            set;
+        }
+
+        public String controlsForFiltered
+        {
+            get;
+            set;
+        }
+
+        public String describedbyFiltered
+        {
+            get;
+            set;
+        }
+
+        
+        public String flowtoFiltered
+        {
+            get;
+            set;
+        }
+
+        public Boolean? invalidFiltered
+        {
+            get;
+            set;
+        }
+
+        public String labelledbyFiltered
+        {
+            get;
+            set;
+        }
+
+        public Boolean? liveFiltered
         {
             get;
             set;
         }
 
 
-        public Boolean? IsEnabled
-        {// Boolean? => true, false, null
+        public Boolean? multiselectedtableFiltered
+        {
             get;
             set;
         }
-    }
+
+        public Boolean? readonlyFiltered
+        {
+            get;
+            set;
+        }
+
+        public Boolean? requiredFiltered
+        {
+            get;
+            set;
+        }
+
+        public Boolean? secretFiltered
+        {
+            get;
+            set;
+        }
+
+        public int valuemaxFiltered
+        {
+            get;
+            set;
+        }
+
+        public int valueminFiltered
+        {
+            get;
+            set;
+        }
+
+        public String valuenowFiltered
+        {
+            get;
+            set;
+        }
+
+
+       
+
+
+
 }
+}
+
