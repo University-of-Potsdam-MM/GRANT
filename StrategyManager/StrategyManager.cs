ffae7dc933691fb6e62e063ad64e3dbed435b7a9
mazandar <mazandar@uni-potsdam.de>
2016-05-02 12:06:11 +0200

    Exception-Behandlung ergänzt

--- StrategyManager/StrategyManager.cs
+++ StrategyManager/StrategyManager.cs
@@ -19,11 +19,26 @@ namespace StrategyManager
 
         public void setSpecifiedFilter(String filterName)
         {
-            Type type = Type.GetType(filterName);
-            specifiedFilter =  (IFilterStrategy)Activator.CreateInstance(type);
-            specifiedFilter.setSpecifiedOperationSystem(specifiedOperationSystem);
-            specifiedFilter.setSpecifiedTree(specifiedTree);
+            try {
+                Type type = Type.GetType(filterName);
+                specifiedFilter = (IFilterStrategy)Activator.CreateInstance(type);
+                specifiedFilter.setSpecifiedOperationSystem(specifiedOperationSystem);
+                specifiedFilter.setSpecifiedTree(specifiedTree);
+            }
+            
+            catch (InvalidCastException ic)
+            {
+                throw new InvalidCastException("Fehler bei StrategyManager_setSpecifiedFilter: " + ic.Message);
+            }
+            catch (ArgumentException ae)
+            {
+                throw new ArgumentException("Fehler bei StrategyManager_setSpecifiedFilter: " + ae.Message);
 
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Fehler bei StrategyManager_setSpecifiedFilter: " + e.Message);
+            }
         }
         
         public IFilterStrategy getSpecifiedFilter()
@@ -34,8 +49,23 @@ namespace StrategyManager
          
         public void setSpecifiedOperationSystem(String operationSystemName)
         {
-            Type type = Type.GetType(operationSystemName);
-            specifiedOperationSystem = (IOperationSystemStrategy)Activator.CreateInstance(type);
+            try {
+                Type type = Type.GetType(operationSystemName);
+                specifiedOperationSystem = (IOperationSystemStrategy)Activator.CreateInstance(type);
+            }
+            catch (InvalidCastException ic)
+            {
+                throw new InvalidCastException("Fehler bei StrategyManager_setSpecifiedFilter: " + ic.Message);
+            }
+            catch (ArgumentException ae)
+            {
+                throw new ArgumentException("Fehler bei StrategyManager_setSpecifiedFilter: " + ae.Message);
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Fehler bei StrategyManager_setSpecifiedFilter: " + e.Message);
+            }
 
         }
 
@@ -48,19 +78,37 @@ namespace StrategyManager
 
         public void setSpecifiedTree(String treeName)
         {
-            Type type = Type.GetType(treeName);
-            Type[] typeArgs = { typeof(GeneralProperties) };
-            var makeme = type.MakeGenericType(typeArgs);
-            specifiedTree = (ITreeStrategy<GeneralProperties>)Activator.CreateInstance(makeme);
+            try {
+                Type type = Type.GetType(treeName);
+                Type[] typeArgs = { typeof(GeneralProperties) };
+                var makeme = type.MakeGenericType(typeArgs);
+                specifiedTree = (ITreeStrategy<GeneralProperties>)Activator.CreateInstance(makeme);
+            }
+            catch (InvalidCastException ic)
+            {
+                throw new InvalidCastException("Fehler bei StrategyManager_setSpecifiedFilter: " + ic.Message);
+            }
+            catch (ArgumentException ae)
+            {
+                throw new ArgumentException("Fehler bei StrategyManager_setSpecifiedFilter: " + ae.Message);
 
-
-
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Fehler bei StrategyManager_setSpecifiedFilter: " + e.Message);
+            }
         }
 
         public void setSpecifiedTree(ITreeStrategy<GeneralProperties> treeName)
         {
-            treeName.NewNodeTree();
-            specifiedTree = treeName;
+            try {
+                treeName.NewNodeTree();
+                specifiedTree = treeName;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Fehler bei StrategyManager_setSpecifiedFilter: " + e.Message);
+            }
         }
 
         public ITreeStrategy<GeneralProperties> getSpecifiedTree()
