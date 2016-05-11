d557f49d87e26b5a917b90b610ad366137f8be89
Marlene Luisa Karlapp <marlene.karlapp@uni-potsdam.de>
2016-05-11 15:48:31 +0200

    Vergessene Datein

---
+++ StrategyManager/XMLSerialize.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace StrategyManager
+{
+    public class XMLSerialize
+    {
+        public static XMLDevice  XMLDeserialize(String filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(XMLDevice));
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            XmlReader reader = XmlReader.Create(fs);
+
+            XMLDevice myXML = (XMLDevice)serializer.Deserialize(reader);
+            fs.Close();
+            return myXML;
+        }
+    }
+}
